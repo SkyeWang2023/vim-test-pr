890d6a56e85ea88993960b59434e42b1c176a3c1
caiyunw <wangcaiyun198600@163.com>
2026-04-01 17:44:01 +0800

    Revert "patch 8.2.4233: crash when recording and using Select mode"
    This reverts commit a4bc2dd7cccf5a4a9f78b58b6f35a45d17164323.

--- src/getchar.c
+++ src/getchar.c
@@ -252,11 +252,8 @@ add_buff(
     static void
 delete_buff_tail(buffheader_T *buf, int slen)
 {
-    int len;
+    int len = (int)STRLEN(buf->bh_curr->b_str);
 
-    if (buf->bh_curr == NULL || buf->bh_curr->b_str == NULL)
-	return;  // nothing to delete
-    len = (int)STRLEN(buf->bh_curr->b_str);
     if (len >= slen)
     {
 	buf->bh_curr->b_str[len - slen] = NUL;
--- src/version.c
+++ src/version.c
@@ -751,8 +751,6 @@ static char *(features[]) =
 static int included_patches[] =
 {   /* Add new patch number below this line */
 /**/
-    4233,
-/**/
     4232,
 /**/
     4231,
